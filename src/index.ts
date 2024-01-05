07b37e53c3aab0d14c5ac5674804ce5d9e958b78
Artem_Hvedinich <artem33771@gmail.com>
2024-01-05 15:54:19 +0300

    Add lib in git

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+export const instance = axios.create({
+    baseURL: "/api/v2/",
+    headers: {
+        "content-type": "application/json",
+        Accept: "application/json",
+    },
+});
+export type LoginResponseType = {
+    data: {
+        accessToken?: string,
+        ok: boolean,
+        refreshToken?: string,
+        msg?: string,
+        code?: number
+    }
+}
+
+export const authentication = (credential: string, password: string): Promise<LoginResponseType> => instance.post("login", {
+    credential,
+    password
+})
+
+export const isAuth = (() => !!localStorage.getItem("accessToken"))()
+
+
